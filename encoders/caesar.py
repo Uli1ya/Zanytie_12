27b0dea4df76fefb7216ee168cd2b3b8cd498baf
Yulia <geoyul@ya.ru>
2024-07-03 19:49:26 +0300

    Create implementation for vigenere. Added base_encoder

---
+++ encoders/caesar.py
@@ -0,0 +1,17 @@
+from common import shift
+from encoders.base_encoder import BaseEncoder
+
+
+class Caesar(BaseEncoder):
+    @BaseEncoder.key.setter
+    def key(self, new_key: int):
+        if not isinstance(new_key, int):
+            raise AttributeError(f'Ключом может быть только целое число, но передан "{new_key}"')
+        self._key = new_key
+
+    def encode(self, text: str) -> str:
+        return "".join(shift(ch, self.key) for ch in text)
+
+
+    def decode(self, text: str, key: int = 3) -> str:
+        return "".join(shift(ch, -self.key) for ch in text)
